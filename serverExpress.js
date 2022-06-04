b12de1808fdd8fdbfcb3dd667e03aef62e0df835
DiscordianWeb <Discordiannest@gmail.com>
2022-06-04 09:55:45 +0100

    Adding routing and middleware

--- serverExpress.js
+++ serverExpress.js
@@ -11,6 +11,44 @@ app.get('/', function(req, res) {
 res.sendFile(__dirname + '/index.html');
 });
 
+
+
+// ########## ADMIN ROUTE ###############################################################
+//get an instance of the router
+var adminRouter = express.Router();
+
+// admin main page. the dashboard (http://localhost:PORT/admin)
+adminRouter.get('/', function(req, res) {
+res.send('I am the dashboard!'); });
+
+// ~~~~~~~~~~ MIDDLEWARE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next) {
+	
+// log each request to the console
+console.log(req.method, req.url);
+
+// continue doing what we were doing and go to the route
+next(); });
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// users page (http://localhost:PORT/admin/users)
+adminRouter.get('/users', function(req, res) {
+res.send('I show all the users!'); });
+
+// posts page (http://localhost:PORT/admin/posts)
+adminRouter.get('/posts', function(req, res) {
+res.send('I show all the posts!'); });
+
+// apply the routes to our application
+app.use('/admin', adminRouter);
+
+// #######################################################################################
+
+
+
+
+
 // start the server
 app.listen(PORT);
 console.log('Express Server running at http://127.0.0.1:'.PORT);
